86dc9fdcbffd1f83aeaddd60b40ca35f3032d92b
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2024-07-10 22:18:20 +0300

    chore: create vite

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import {resolve} from 'path'
+
+export default defineConfig({
+  root: resolve(__dirname, 'src'),
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
+  resolve: { 
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }, 
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.html'),
+        catalog: resolve(__dirname, 'src/catalog.html')
+      }
+    }
+}
+})
