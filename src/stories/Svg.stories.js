a9676f0d1df286946228286b9106008744306602
Evgeniya Makeeva <makeevaeva91@gmail.com>
2024-07-25 11:50:41 +0300

    feat: added svg images to storybook

---
+++ src/stories/Svg.stories.js
@@ -0,0 +1,12 @@
+import { iconUser } from '../shared/img/svg/user.svg';
+import { iconDownload } from '../shared/img/svg/download.svg';
+
+
+export default {
+    title: 'System/Icons',
+};
+
+export const icons = [
+     {name: "User", icon: iconUser },
+     {name: "Download", icon: iconDownload}
+] 
