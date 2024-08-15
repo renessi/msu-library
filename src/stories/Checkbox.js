8dd26559fd927d6fb67f05b65ae72cedf894fbed
Evgeniya Makeeva <makeevaeva91@gmail.com>
2024-08-15 12:51:42 +0300

    feat: init checkbox component in to shared/ui

--- src/stories/Checkbox.js
+++ src/stories/Checkbox.js
@@ -1,10 +1,10 @@
-import "./checkbox.css"
+import "../06_shared/ui/checkbox/checkbox.scss"
 
 export const createCheckbox = ({ checked = false}) => {
     const check = checked ? "checked" : "";
 
 
-return  `<div class="ckeckbox"><input type="checkbox" class="checkbox" ${check}><label class="checkbox"assets
+return  `<div><input type="checkbox" class="checkbox" ${check}><label assets
 <svg class="checkbox" width="25" height="25" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg" fill="none">
 <path d="M19 5L8.6875 16L4 11" stroke="url(#paint0_linear_176_9088)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 <defs>
