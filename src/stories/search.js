9af4e8afebe64ffec4abddd96e3004f9938e5906
catherinsmi <catherinsmi@yandex.ru>
2024-08-27 13:29:18 +0300

    refactor: fixes import

--- src/stories/search.js
+++ src/stories/search.js
@@ -5,26 +5,16 @@ import iconUser from "../06_shared/img/svg/arrow-right.svg";
 export const createSearch = ({label}) => {
     //Создаем элементы разметки для search
     const search = document.createElement("search");
-    const form = document.createElement("form");
-    form.classList.add('search');
-    const input = document.createElement("input");
-    input.classList.add('search__input');
-    input.type = "search";
-    input.placeholder = label 
-    const span = document.createElement("span");
-    span.classList.add('search__round');
 
-    //Создаем элементы svg и use, импортируем пространство имен для тэгов и атрибутов
-    const searchIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    const useElement = document.createElementNS("http://www.w3.org/2000/svg", "use");
-    useElement.setAttributeNS("http://www.w3.org/1999/xlink", "href", "#icon-arrow-right");
-
-    //Создаем архитектуру узлов разметки
-    searchIcon.appendChild(useElement);
-    span.appendChild(searchIcon);
-    search.append(form)
-    form.prepend(input)
-    form.append(span)
-
+    search.innerHTML = `
+        <form class="search">
+            <input class="search__input" type="search" placeholder="${label}">
+            <button class="search__round">
+            <svg width="24" height="24">
+                <use xlink:href="#icon-arrow-right"></use>
+            </svg>
+            </button>
+        </form>
+        `;
     return search
 }
