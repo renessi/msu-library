2e1948a4061b4af3d8974951467cb00ffb96e729
m.belogubova <m.belogubova@webvork.com>
2024-08-30 12:52:09 +0300

    fix: checkbox link and styles for storybook

--- src/stories/Checkbox.stories.js
+++ src/stories/Checkbox.stories.js
@@ -4,22 +4,25 @@ import { createCheckbox } from './Checkbox';
 export default {
   title: "Component/Checkbox",
   tags: ["autodocs"],
-  render: (args) => {
-    return createCheckbox(args);
+  render: ({ label, ...args }) => {
+    return createCheckbox({ label, ...args });
   },
   argTypes: {
     checked: {control: "boolean"},
+    label: { control: "text" }
   },
 };
 
 export const Default = {
   args: {
     checked: false,
+    label: 'Label Text'
   },
 };
 
 export const Checked = {
   args: {
     checked: true,
+    label: 'Label Text'
   }
 };
