15f5ae94207e311c9d99406cd6b375bb12af2079
Evgeniya Makeeva <makeevaeva91@gmail.com>
2024-08-30 15:37:34 +0300

    71605263-feat: implement 2 state for filter panel

--- src/stories/Checkbox.js
+++ src/stories/Checkbox.js
@@ -4,8 +4,8 @@ export const createCheckbox = ({ checked = false, label}) => {
     const check = checked ? "checked" : "";
 
 
-return  `<div><input type="checkbox" class="checkbox" ${check}><label assets
-<svg class="checkbox__img" width="25" height="25" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg" fill="none">
-<path d="M19 5L8.6875 16L4 11" stroke="url(#paint0_linear_176_9088)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>`
+return  `<div style="background: rgba(0,0,0,0.7);"><label class="checkbox">
+         <input class="checkbox__input" type="checkbox" ${check}> 
+         <span class="checkbox__span">${label}</span>
+         </label></div>`
 }
