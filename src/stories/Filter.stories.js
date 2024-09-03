0016e0caec0cb349fb66478ab6b909cd0f8562ef
Evgeniya Makeeva <makeevaeva91@gmail.com>
2024-09-03 18:04:38 +0300

    fix: storybook and structure

---
+++ src/stories/Filter.stories.js
@@ -0,0 +1,18 @@
+import { createFilter } from "./filter";
+
+export default {
+    title: "Component/Filter",
+    tags: ["autodocs"],
+    render: ({ label}) => {
+        return createFilter({ label});
+    },
+    argTypes: {
+        label: { control: "text" },
+    },
+};
+
+export const DefaultFilter = {
+    args: {
+        label: "Поиск",
+    },
+};
---
+++ src/stories/filter.js
@@ -0,0 +1,33 @@
+import IconUp from '@/06_shared/img/svg/chevron-up.svg';
+import IconDown from '@/06_shared/img/svg/chevron-down.svg';
+import IconSearch from '@/06_shared/img/svg/search.svg';
+
+export const createFilter = ({label}) => {
+
+    const filter = document.createElement("div");
+
+    filter.innerHTML = `
+       <div style="background: rgba(0, 0, 0, 1)">
+        <div class="filter">
+          <div class="filter__subject">
+            <span class="filter__text">Предмет</span>
+            <svg class="filter__img">
+              <use xlink:href="#icon-chevron-up"></use>
+            </svg>
+          </div>
+          <div class="filter__search">
+            <svg class="filter__search-img">
+              <use xlink:href="#icon-search"></use>
+            </svg>
+            <form action="" class="filter__form">
+              <input class="filter__input" type="text">
+            </form>
+            <svg class="filter__img">
+              <use xlink:href="#icon-chevron-down"></use>
+            </svg>
+          </div>
+        </div> `;
+
+        return filter
+
+}
