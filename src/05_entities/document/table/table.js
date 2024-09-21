bd8b9f4036f465b7e0c44a5d488b5fc864b7cd0a
Christina <stina.novickova@yandex.ru>
2024-09-21 18:06:43 +0300

    fix: add download icon and autosize for columns

--- src/05_entities/document/table/table.js
+++ src/05_entities/document/table/table.js
@@ -1,3 +1,5 @@
+import iconDownload from "@/06_shared/img/svg/download.svg";
+
 const gridTable = () => {
     const filterParamsText =  {
         filterOptions:['contains'],
@@ -12,21 +14,34 @@ const gridTable = () => {
     }
     const gridOptions = {
         rowData: [
-            { file: "Теоремы и задачи функана Кириллов Гвишиани.pdf", discipline: "Функциональный анализ", type: "Литература", year: 1988, author: "", download: "" },
+            { file: "Теоремы и задачи функана Кириллов Гвишиани.pdf", discipline: "Функциональный анализ", type: "Литература", year: 1988, author: "", download: `` },
             { file: "Программа Экзамена Булинский", discipline: "Случайные процессы", type: "Экзамен", year: 2024, author: "Булинский А.В.", download: "" },
             { file: "Видеолекции youtube Булинский", discipline: "Случайные процессы", type: "Другие", year: 2018, author: "Булинский А.В.", download: "" },
             { file: "Лабораторная  Половинко 2019", discipline: "Уравнения мат. физики", type: "Лабораторная", year: 2019, author: "Половинко А.С.", download: "" },
             { file: "УМФ КР2 411 группа.png", discipline: "Уравнения мат. физики", type: "КР", year: 2019, author: "Половинко А.С.", download: "" }
         ],
         columnDefs: [
-            { field: "file", flex: 3, checkboxSelection: true, filter: true, filterParams: filterParamsText },
-            { field: "discipline", flex: 3, filter: true, filterParams: filterParamsText },
-            { field: "type", flex: 3, filter: true, filterParams: filterParamsText },
-            { field: "year", flex: 2, filter: 'agNumberColumnFilter',filterParams: filterParamsNumber },
-            { field: "author",flex: 3, filter: true, filterParams: filterParamsText },
-            { field: "download", flex: 1 }
+            { field: "file", flex: 4, checkboxSelection: true, filter: true, filterParams: filterParamsText },
+            { field: "discipline", filter: true, filterParams: filterParamsText },
+            { field: "type", filter: true, filterParams: filterParamsText },
+            { field: "year", filter: 'agNumberColumnFilter',filterParams: filterParamsNumber },
+            { field: "author",filter: true, filterParams: filterParamsText },
+            { field: "download",
+                cellRenderer: function() {
+                    return `
+                        <svg class="table-img">
+                            <use xlink:href="#icon-download"></use>
+                        </svg>
+                    `
+                }
+            },
         ],
         rowSelection: 'multiple',
+        onGridReady: (params) => {
+            params.api.sizeColumnsToFit();
+            const columnsToAutoSize = ['discipline', 'type', 'year', 'author', 'download'];
+            params.columnApi.autoSizeColumns(columnsToAutoSize);
+        }
     }
     return {
         init: (parentDiv) => {
