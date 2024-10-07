74714fd0a09444b2eb40b5aca59e288986f709f1
m.belogubova <m.belogubova@webvork.com>
2024-10-07 11:22:02 +0300

    feature: filter open action

--- src/04_features/filter/ui/filter-container.js
+++ src/04_features/filter/ui/filter-container.js
@@ -1,14 +1,45 @@
-const filterContainer = document.querySelector('.filter__container');
-const filters = filterContainer.querySelectorAll('.filter__top');
+const filterList = document.querySelector('#filterList');
+const actions = {
+    openSearch: openSearch,
+    toggleList: toggleList
+}
+filterList.addEventListener('click', handleFilterClick )
 
-filters.forEach(filter => {
-    const down = filter.querySelector('.filter__img');
-    const menu = filter.querySelector('.filter__menu');
+function handleFilterClick(event) {
+    // debugger
+    console.dir(event.target)
+    const dataset = event.target.dataset
+    const closestTarget = event.target.closest('[data-filter]')
+    console.log(closestTarget)
+    if(dataset.filter && actions[dataset.filter]) {
+        const filterNode = event.target.closest('[data-filter="item"]')
+        actions[dataset.filter].call(null, filterNode) //actions[dataset.filter]()
+    } else if(closestTarget && actions[closestTarget.dataset.filter]) {
+        const filterNode = event.target.closest('[data-filter="item"]')
+        actions[closestTarget.dataset.filter].call(null, filterNode)
+    }
+}
+
+function openSearch(filterNode) {
+    console.log('openSearch')
+    // filterNode.classList.toggle('--search')
+}
+
+function toggleList(filterNode) {
+    console.log(filterNode, 'toggleList')
+    filterNode.classList.toggle('--open')
+    // down.classList.toggle('filter__img--rotate');
+    // menu.classList.toggle('filter__menu--open');
+
+}
+// filters.forEach(filter => {
+//     const down = filter.querySelector('.filter__img');
+//     const menu = filter.querySelector('.filter__menu');
     
-    filter.addEventListener('click', () => {
-        down.classList.toggle('filter__img--rotate');
-        menu.classList.toggle('filter__menu--open');
-    });
-});
-console.log(filters);
+//     filter.addEventListener('click', () => {
+//         down.classList.toggle('filter__img--rotate');
+//         menu.classList.toggle('filter__menu--open');
+//     });
+// });
+// console.log(filters);
 
