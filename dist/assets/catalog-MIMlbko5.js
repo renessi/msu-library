96373f948d2f13bbc1b864608913e722407e4604
m.belogubova <m.belogubova@webvork.com>
2024-12-02 11:47:44 +0300

    feat: search and sidebar are working together

--- dist/assets/catalog-MIMlbko5.js
+++
@@ -1,1 +0,0 @@
-import{d as m}from"./main-C_DFApaY.js";m();
--- src/04_features/search/search.js
+++ src/04_features/search/search.js
@@ -1,5 +1,6 @@
 import { getSearchedDocumentsToTable } from '@/04_features/documents/documents.js';
 import { documentUpdate } from '@/05_entities/document/table/table.js'
+import store from '@/01_app/Store.js';
 
 const docSearchNode = document.querySelector('#documentSearch');
 const docSearchInputNode = document.querySelector('#documentSearchInput');
@@ -11,12 +12,17 @@ let ePrevValue
 async function handleSubmitDocumentSearch(e) {
     e.preventDefault()
     const formValues = Object.fromEntries(new FormData(e.target));
-    const docResponse = await getSearchedDocumentsToTable({prompt: formValues.search})
+    store.setSearchValue(formValues.search)
+    const docResponse = await getSearchedDocumentsToTable()
     await documentUpdate(docResponse)
 }
 async function handleInputDocumentSearch(e) {
     const eValue = e.target.value.trim()
-    if(!eValue && eValue !== ePrevValue) documentUpdate()
+    if(!eValue && eValue !== ePrevValue) {
+        store.setSearchValue('')
+        const docResponse = await getSearchedDocumentsToTable()
+        await documentUpdate(docResponse)
+    } 
     ePrevValue = e.target.value.trim()
 }
 
